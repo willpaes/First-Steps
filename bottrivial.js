10e3fa44ae81888648067094f83ab98da7861310
willpaes <77067602+willpaes@users.noreply.github.com>
2026-04-01 19:32:16 -0300

    Add bottrivial.js for coding fun facts

---
+++ bottrivial.js
@@ -0,0 +1,29 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Fabio";
+let botLocation = "Planaltina";
+let favoriteLanguage = "Javascript";
+
+const defaultAnswer = "My name is " + botName + " and I
+live on " + botLocation + ".";
+console.log(defaultAnswer);
+
+const continueAnswer = "My favorite programming
+language is " + favoriteLanguage + ".";
+console.log(continueAnswer);
+
+let codingFact = "That's a fun fact about your bot's
+favorite coding language " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = 'My favorite hobby is coding with ' +
+favoriteLanguage +'.';
+console.log(codingFact);
+
+codingFact = "The new Friendly Neighbourhood is " +
+favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you.
+Goodbye!" + " - " + botName + " from " + botLocation +
+".");
